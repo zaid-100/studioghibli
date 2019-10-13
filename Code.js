2018ba97c5694e6717b8de9830a1067651542d8f
Alexandra M. Boardman <alexandra.boardman@centre.edu>
2019-10-13 16:45:19 -0400

    Before meeting with B

--- Code.js
+++ Code.js
@@ -5,20 +5,13 @@ filPromise.then(
 function(data)
     {
         nlist(data);
+        movtitle(data);
         infodes(data);
         infodir(data);
         infopro(data);
         inforel(data);
     console.log("works",data);
     })
-
-
-
-
-
-
-
-
 
 
 var nlist = function(data)
@@ -29,39 +22,68 @@ var nlist = function(data)
     .data(data)
     .enter()
     .append("li")
-    .on ("mouseover", function(data))
-    {
     .append("div")
     .attr("class" ,"films")
-    .text (function(data)
-          {return d.title})
-         }
     }
+
+var movtitle = function(data)
+{
+    d3.selectAll(".films").append("div").attr("class", "movietitle")
+.text(function(d) {return d.title})
+}
+
+
 
 var infodes = function(data)
 {
     d3.selectAll(".films").append("div").attr("class", "infodes")
-.text(function(d) {return d.description})
-}
+.text (function(d) {return "Description: " + d.description}
+)}
 
 
 var infodir = function(data)
 {
     d3.selectAll(".films").append("div").attr("class", "infodir")
-.text(function(d) {return d.director})
+.text(function(d) {return "Director: " + d.director})
 }
 
 
 var infopro = function(data)
 {
     d3.selectAll(".films").append("div").attr("class", "infopro")
-.text(function(d) {return d.producer})
+.text(function(d) {return "Producer: " + d.producer})
 }
 
 var inforel = function(data)
 {
     d3.selectAll(".films").append("div").attr("class", "inforel")
-.text(function(d) {return d.release_date})
+.text(function(d) {return "Release Date: " + d.release_date})
     
 
 }
+
+
+
+
+
+d3.json("https://ghibliapi.herokuapp.com/people/")
+var filPromise=d3.json("https://ghibliapi.herokuapp.com/people/");
+filPromise.then(
+function(data)
+    {
+        displaypeople(data)
+        console.log("gothere")
+    })
+
+var displaypeople = function(data)
+    { 
+        d3.select("film")
+        .data(data)
+        .enter()
+        .append("div")
+        .attr("class", "people")
+        .text(function(data) {return data.name})
+    }
+    
+    
+    
